ab6aabe40f87a63cab3ea5a550272030471632dd
Abdur Rehman <abdurrehman112919@gmail.com>
2024-08-01 14:54:33 +0500

    Updated Storing Data and Displaying using Array

--- Task_No_6.cpp
+++ Task_No_6.cpp
@@ -1,100 +1,133 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class Animal
-{private:
-	string name;
-	int id;
-	string habitat;
-public:
-	Animal(string n = "", int i = 0, string h = "") { name = n;id = i;habitat = h; }
-	void displayDetails()
-	{
-		cout << "\nAnimal Details"<<endl;
-			cout<<"Animal Name : "<<name<< ",\tAnimal ID : " << id<< "\tAnimal Habitat : " << habitat;
-	}
-	void modifyDetails()
-	{
-		cout << "Enter Animal Name : ";
-		cin >> name;
-		cout << "Enter Animal ID : ";
-		cin >> id;
-		cout << "Enter Animal Habitat : ";
-		cin >> habitat;
-	}
-};
-class Mammal :public Animal
 {
-	string dietType;
+private:
+    string name;
+    string id;
+    string habitat;
+
 public:
-	Mammal(string n="", int i=0, string h="", string dT="") :Animal(n, i, h)
-	{
-		dietType = dT;
-	}
-	void displayDetails()
-	{
-		Animal::displayDetails();
-		cout << "\tAnimal Diet Type : " << dietType;
-	}
-	void modifyDetails()
-	{
-		Animal::modifyDetails();
-		cout << "Enter Diet Type (Herbivore/Carnivore/omnivore) : ";
-		cin >> dietType;
-	}
+    Animal(string n = "", string i = "", string h = "")
+    {
+        name = n;
+        id = i;
+        habitat = h;
+    }
+    void displayDetails()
+    {
+        cout << "\nAnimal Name : " << name << ",\nAnimal ID : " << id << "\nAnimal Habitat : " << habitat;
+    }
+    void modifyDetails()
+    {
+        cout << "Enter Animal Name : ";
+        cin >> name;
+        cout << "Enter Animal ID : ";
+        cin >> id;
+        cout << "Enter Animal Habitat : ";
+        cin >> habitat;
+    }
+};
+class Mammal : public Animal
+{
+    string dietType;
+
+public:
+    Mammal(string n = "", string i = "", string h = "", string dT = "") : Animal(n, i, h)
+    {
+        dietType = dT;
+    }
+    void displayDetails()
+    {
+        cout << "\nMammal Details";
+        Animal::displayDetails();
+        cout << "\nAnimal Diet Type : " << dietType;
+    }
+    void modifyDetails()
+    {
+        Animal::modifyDetails();
+        cout << "Enter Diet Type (Herbivore/Carnivore/omnivore) : ";
+        cin >> dietType;
+    }
 };
 class Bird : public Animal
 {
-	string wingSpan;
+    string wingSpan;
+
 public:
-	Bird(string n = "", int i = 0, string h = "", string wS="") :Animal(n, i, h)
-	{
-		wingSpan = wS;
-	}
-	void displayDetails()
-	{
-		Animal::displayDetails();
-		cout << "\tAnimal Wing Span : " << wingSpan<<" meter";
-	}
-	void modifyDetails()
-	{
-		Animal::modifyDetails();
-		cout << "Enter Wing Span in meters :";
-		cin >> wingSpan;
-	}
+    Bird(string n = "", string i = "", string h = "", string wS = "") : Animal(n, i, h)
+    {
+        wingSpan = wS;
+    }
+    void displayDetails()
+    {
+        cout << "\nBird Details";
+        Animal::displayDetails();
+        cout << "\nAnimal Wing Span : " << wingSpan << " meter";
+    }
+    void modifyDetails()
+    {
+        Animal::modifyDetails();
+        cout << "Enter Wing Span in meters :";
+        cin >> wingSpan;
+    }
 };
 int main()
 {
-	Mammal mammal;
-	Bird bird;
-	while (true)
-	{
-		char animal_type;
-		cout << "Add new Animal (Mammal/Bird)? (M/B): ";
-		cin >> animal_type;
-		if (tolower(animal_type) != 'm' && tolower(animal_type) != 'b')
-		{
-			cout << "Invalid Input! Enter M for Mammal and B for Bird\n";
-			continue;
+    
 
-		}
-		else if (tolower(animal_type) == 'm')
-		{
-			mammal.modifyDetails();
-		}
-		else if (tolower(animal_type) == 'b')
-		{
-			bird.modifyDetails();
-		}
-		char add_new;
-		cout << "Add new Animal (Yes/No)? (Y/N): ";
-		cin >> add_new;
-		if (tolower(add_new) != 'y')
-			break;
-	}
+        
+        const int AllAnimal=100;
+        Mammal mammal[AllAnimal];
+        Bird bird[AllAnimal];
 
-	
+        int newMammal = 0;
+        int newBird = 0;
+        while (true)
+        {
+            char animal_type;
+            cout << "Add new Animal (Mammal/Bird)? (M/B): ";
+            cin >> animal_type;
+            if (tolower(animal_type) != 'm' && tolower(animal_type) != 'b')
+            {
+                cout << "Invalid Input! Enter M for Mammal and B for Bird\n";
+                continue;
+            }
+            else if (tolower(animal_type) == 'm')
+            {
+                if (newMammal < AllAnimal)
+                {
+                    mammal[newMammal].modifyDetails();
+                    newMammal++;
+                }
+            }
+            else if (tolower(animal_type) == 'b')
+            {
+                if (newBird < AllAnimal)
+                    bird[newBird].modifyDetails();
+                newBird++;
 
-	return 0;
+            }
+            char add_new;
+            cout << "Add new Animal (Yes/No)? (Y/N): ";
+            cin >> add_new;
+            if (tolower(add_new) != 'y')
+                break;
+        }
+
+        
+        for (int i = 0;i < newMammal;i++)
+        {
+            mammal[i].displayDetails();
+        }
+        cout << "\n";
+        for (int i = 0;i < newBird;i++)
+        {
+            bird[i].displayDetails();
+        }
+        return 0;
+    
 }
